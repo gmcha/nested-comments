b83fe32c353748b9c3288ddb58e5daea349a53d6
Ga Min Cha <prettygamin@naver.com>
2025-11-26 14:01:10 +0900

    React Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import { PostList } from "./components/PostLists"
 
 function App() {
-  return <PostList />
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/posts/:id" element={<h1>Post</h1>} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
