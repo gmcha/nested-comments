9dc2f974256769cf7774ded9002790ace93bd964
Ga Min Cha <prettygamin@naver.com>
2025-11-29 17:32:30 +0900

    create useAsync custorm hook

--- client/src/components/PostLists.js
+++ client/src/components/PostLists.js
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useAsync } from "../hooks/useAsync"
 import { getPosts } from "../services/posts"
 
 export function PostList() {
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        getPosts().then(setPosts)
-    }, [])
+    const { loading, error, value: posts } = useAsync(getPosts)
+
+    if (loading) return <h1>Loading</h1>
+    if (error) return <h1 className="error-msg">{error}</h1>
+
     return posts.map(post => {
         return (
             <h1 key={post.id}>
