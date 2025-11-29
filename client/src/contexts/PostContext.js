5a42b0a05949a3d3163007b4eacbe65f05c89823
Ga Min Cha <prettygamin@naver.com>
2025-11-29 22:29:04 +0900

    render comments

--- client/src/contexts/PostContext.js
+++ client/src/contexts/PostContext.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react"
+import React, { useContext, useMemo } from "react"
 import { useParams } from "react-router-dom"
 import { useAsync } from "../hooks/useAsync"
 import { getPost } from "../services/posts"
 
 const Context = React.createContext()
 
+export function usePost() {
+    return useContext(Context)
+}
+
 export function PostProvider({ children }) {
     const { id } = useParams()
     const { loading, error, value: post } = useAsync(() => getPost(id), [id])
+    const commentsByParentId = useMemo(() => {
+        if (post?.comments == null) return []
+        const group = {}
+        post.comments.forEach(comment => {
+            group[comment.parentId] ||= []
+            group[comment.parentId].push(comment)
+        })
+        return group
+    }, [post?.comments])
+
+    function getReplies(parentId) {
+        return commentsByParentId[parentId]
+    }
     
     return (<Context.Provider 
         value={{
             post: {id, ...post},
+            rootComments: commentsByParentId[null],
+            getReplies,
         }}
     >
         {loading ? (
